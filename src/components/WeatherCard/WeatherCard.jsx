351dfba3831bb8d5f695de2677ceec91db586cf2
Samantha P <samantha.paras94@gmail.com>
2026-01-04 19:32:00 -0600

    Render and Filter Cards

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,13 @@
+import "./WeatherCard.css";
+import sunny from "../../images/sunny.png";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp"> 75 &deg;F</p>
+      <img src={sunny} alt="Sunny Weather" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
